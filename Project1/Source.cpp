3d20b3e587325da5e65c90792679b882b242333c
zakharSen <139217440+zakharSen@users.noreply.github.com>
2023-09-27 16:59:55 +0300

    FirstCom

---
+++ Project1/Source.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double m; 
+	double z1;
+	double z2; 
+	cout << "m = "; cin >> m;
+	z1 = (sin(m) + sin(5 * m) - sin(3 * m)) / (cos(m) - cos(3 * m) + cos(5 * m));
+	z2 = tan(3 * m);
+	cout << endl;
+	cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;
+	cin.get();
+	return 0;
+}

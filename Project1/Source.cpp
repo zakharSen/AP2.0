3de97e0aeb94ba41e818db2609b5315cfbef2926
zakharSen <139217440+zakharSen@users.noreply.github.com>
2023-09-28 12:32:01 +0300

    ThirdCom

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -9,6 +9,14 @@ int main()
 	cout << "m = "; cin >> m;
 	z1 = (sin(m) + sin(5 * m) - sin(3 * m)) / (cos(m) - cos(3 * m) + cos(5 * m));
 	z2 = tan(3 * m);
+	if (z2 <= z1) {
+		z2 = z2 * 100;
+	}
+	else
+	{
+		z2 = z2 / 100;
+	}
+
 	cout << endl;
 	cout << "z1 = " << z1 << endl;
 	cout << "z2 = " << z2 << endl;
